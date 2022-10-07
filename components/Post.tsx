cd34ab332142857fa91d882c4657e08afefd53bc
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-07 10:02:32 +0100

    feat: add comments in post

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,13 +2,20 @@ import { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import Collapse from "@mui/material/Collapse";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import * as API from "../utils/api";
 import { PostProps, CommentProps } from "../utils/props";
 
-const Post = ({ userId, id, title, body }: PostProps) => {
+const Post = ({ id, title, body }: PostProps) => {
   const [comments, setComments] = useState<CommentProps[]>([]);
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const onClick = () => {
+    console.log("clicked!");
+    setExpanded(!expanded);
+  };
 
   useEffect(() => {
     const getComments = async () => {
@@ -31,9 +38,26 @@ const Post = ({ userId, id, title, body }: PostProps) => {
         <Typography variant="body2">{body}</Typography>
       </CardContent>
       {comments.length > 0 ? (
-        <CardActions>
-          <Button size="small">{comments.length}</Button>
-        </CardActions>
+        <>
+          <CardActions>
+            <Button onClick={onClick} size="small">
+              {comments.length}
+            </Button>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <Typography variant="h6" align="center">
+              Comments
+            </Typography>
+            {comments.map((comment) => (
+              <CardContent sx={{ my: 4, mx: 4 }}>
+                <Typography>{comment.body}</Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  by: {comment.name}
+                </Typography>
+              </CardContent>
+            ))}
+          </Collapse>
+        </>
       ) : (
         <div></div>
       )}
